400f95aaf54807dac57fdff459dfbf276d934499
David Rupnik <davidrupnik23@gmail.com>
2022-03-05 22:17:56 +0100

    Bump to version 0.2.2 (TerraPlusPlus 1.1.0.627+)

--- src/main/java/com/davixdevelop/terraimprovedbiomes/TerraBiomeMod.java
+++ src/main/java/com/davixdevelop/terraimprovedbiomes/TerraBiomeMod.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = TerraBiomeMod.MODID,
-  dependencies = "required-after:terracommondatasets@[0.2,);required-after:terraplusplus@[0.1.627,)",
+  dependencies = "required-after:terracommondatasets@[0.3,);required-after:terraplusplus@[0.1.627,)",
   acceptableRemoteVersions = "*",
   useMetadata = true)
 public class TerraBiomeMod
